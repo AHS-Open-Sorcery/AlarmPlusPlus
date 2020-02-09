f171f44668e02e2d824d743e415b7396edb9f876
jzhu2020 <jzhu2020@k12.andoverma.us>
2020-02-09 15:48:58 -0500

    Add code to dump form data into text file as JSON

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from flask import *
 from flaskwebgui import FlaskUI  # get the FlaskUI class
+import threading
+import datetime as dt
+import json
 
 app = Flask(__name__)
 ui = FlaskUI(app)  # feed the parameters
-
+alarms_file = open("alarms.txt")
 
 # do your logic as usual in Flask
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/set", methods={"GET", "POST"})
 def set_alarm():
 
     if request.method == "POST":
-        pass
-        # time = request
-        # jacks =
+        time = request.form.get("alarm_time")
+        jacks = request.form.get("number_jacks")
+
+        alarm_time = dt.fromisoformat(time)
+        timer = threading.Timer(alarm_time - dt.datetime.now())
+
+        json.dump((time, jacks), alarms_file)
 
     elif request.method == "GET":
         return render_template('set_alarm.html')
