86f23f23a82336ed5281687c66702de2b991e6ec
jzhu2020 <jzhu2020@k12.andoverma.us>
2020-02-09 15:27:55 -0500

    Set up skeleton for set_alarm()

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ ui = FlaskUI(app)  # feed the parameters
 
 @app.route("/")
 def index():
-    return render_template('set_alarm.html')
+    return render_template('index.html')
+
+@app.route("/set", methods={"GET", "POST"})
+def set_alarm():
+
+    if request.method == "POST":
+        pass
+        # time = request
+        # jacks =
+
+    elif request.method == "GET":
+        return render_template('set_alarm.html')
+
 
 
 ui.run()  # call the 'run' method
